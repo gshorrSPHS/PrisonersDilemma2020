5bacaf2292757179d0ffa3d5be3c47f5f1a9a8ea
LincolnLam1112 <61807386+LincolnLam1112@users.noreply.github.com>
2020-03-06 15:00:09 -0800

    Create team 8.py

---
+++ team 8.py
@@ -0,0 +1,20 @@
+team_name = 'Collusion'
+strategy_name =  'Imitate'
+strategy_description = 'If you betray, we will too.  If you collude, we will too.'
+
+def move(my_history, their_history, my_score, their_score):
+    ''' Arguments accepted: my_history, their_history are strings.
+    my_score, their_score are ints.
+    Make my move.
+    Returns 'c' or 'b'. 
+    '''
+    if len(my_history)==0:
+      return 'b'
+    elif their_history[-2] == 'c' and their_history[-1] == 'c':
+      return 'c'
+    elif their_history[-1] == 'b':
+      return 'b'
+    elif my_score <= their_score:
+      return 'b'
+    elif their_score < my_score:
+      return 'b'
