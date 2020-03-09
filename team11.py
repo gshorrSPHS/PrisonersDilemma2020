b302fb5319d531148ed42436063f809354e3a2d5
yu3254 <61807810+yu3254@users.noreply.github.com>
2020-03-09 14:49:28 -0700

    Update team11.py

--- team11.py
+++ team11.py
@@ -1,6 +1,6 @@
 team_number = 11
 team_name = 'Sibo & Shane'
-strategy_name = 'Follow unless patterns appaer'
+strategy_name = 'Follow unless patterns appear'
 strategy_description = "collude if they colluded last time and betray if they betrayed last time. If last few responses were the same, do the opposite (e.g. 'bbbb' ---> 'c')"
                                                                                                                                   
 def move(my_history, their_history, my_score, their_score):
@@ -9,7 +9,11 @@ choices = ['c','b']
       return 'c'
     elif their_history [-4:] == 'bbbb':
       return 'c'
-    elif their_history [-2:] == 'bb':
+    elif their_history [-4:] == 'cccc':
+      return 'b'
+    elif their_history [-3:] == 'bbb':
+      return 'c'
+    elif their_history [-3:] == 'ccc':
       return 'b'
     elif their_history [-1] == 'c':
       return 'c'
