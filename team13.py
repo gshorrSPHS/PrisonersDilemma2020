25a418cff7396a2e630e3815c9bad02229264670
iDoAllMyHomework <61807484+iDoAllMyHomework@users.noreply.github.com>
2020-03-10 14:16:27 -0700

    Create team13.py
    This is our code

---
+++ team13.py
@@ -0,0 +1,47 @@
+import random 
+
+team_name = 'The Great Depression'
+strategy_name =  'heads collude, tails betray'
+strategy_description = 'The world is cruel. And the only morality in a cruel world is chance.'
+    
+
+
+def move(my_history, their_history, my_score, their_score):
+    ''' Arguments accepted: my_history, their_history are strings.
+    my_score, their_score are ints.
+    Make my move.
+    Returns 'c' or 'b'. 
+    '''
+
+
+    tc = 0
+    tb = 0
+    mc = 0
+    mb = 0
+    for letter in their_history:
+      if letter == "c":
+        tc += 1
+      elif letter == "b":
+        tb += 1
+    for letter in my_history:
+      if letter == "c":
+        mc += 1
+      elif letter == "b":
+        mb += 1
+    if their_score > my_score:
+      if tc > mc: 
+        return "c"  
+      else:
+        return "b"
+        
+        # need to have something here in case nothing else works
+    if their_score < my_score:
+      if tc < mc:
+        return "b"  
+      else:
+        return "c"
+    if their_score == my_score:
+      if tc == mc:
+        return "c"
+      else:
+        return "b"
